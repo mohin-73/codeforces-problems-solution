a5e644cd3410c570c12a1d4e2f688deabea663b2
Eklasur Rahman <mohin73k@gmail.com>
2025-02-08 16:30:19 +0000

    Codeforces 281A. Word Capitalization

---
+++ A/word_capitalization.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    string str;
+    cin >> str;
+    
+    str[0] = toupper(str[0]);
+    cout << str << '\n';
+
+    return 0;
+}
