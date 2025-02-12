a1ac5067af21ea19af592ab53298dd026cbd3cd7
Eklasur Rahman <mohin73k@gmail.com>
2025-02-12 16:26:22 +0000

    Codeforces 112A. Petya and Strings

---
+++ A/petya_and_strings.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    string str1, str2;
+    cin >> str1 >> str2;
+
+    transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
+    transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+
+
+    cout << str1.compare(str2) << '\n';
+    
+    return 0;
+}
