ef729ad9cd8b0c1c0d98f5a1f67a051279417363
Eklasur Rahman <mohin73k@gmail.com>
2025-02-12 16:38:14 +0000

    Codeforces 282A. Bit++

---
+++ A/bit++.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int n, ans = 0;
+    cin >> n;
+
+    while (n--) {
+        string str;
+        cin >> str;
+        if (str[0] == '+' or str[2] == '+') {
+            ans += 1;
+        } else {
+            ans -= 1;
+        }
+    }
+
+    cout << ans << '\n';
+    
+    return 0;
+}
