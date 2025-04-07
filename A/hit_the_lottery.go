89e5a4c2a14a3a70811e89b00ae2194122ce7b4b
Eklasur Rahman <mohin73k@gmail.com>
2025-04-07 09:16:03 +0000

    Codeforces 996A. Hit the Lottery

---
+++ A/hit_the_lottery.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var n int
+    fmt.Scan(&n)
+
+    arr := [5]int{100, 20, 10, 5, 1}
+    p := 0
+    for i := 0; i < 5; i++ {
+        p = p + (n / arr[i])
+        n = n % arr[i]
+    }
+
+    fmt.Println(p)
+}
