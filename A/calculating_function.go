8d0b4f69fa5ed1b56d02d5dafc4fbb7c0757140c
Eklasur Rahman <mohin73k@gmail.com>
2025-04-07 14:44:00 +0000

    Codeforces 486A. Calculating Function

---
+++ A/calculating_function.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var n int64
+    fmt.Scan(&n)
+
+    if n % 2 == 0 {
+        fmt.Println(n / 2)
+    } else {
+        n = (0 - n - 1)
+        fmt.Println(n / 2)
+    }
+}
