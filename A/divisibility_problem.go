971ea0b76f6fc19a3ab9f47f74644ceac6c115d1
Eklasur Rahman <mohin73k@gmail.com>
2025-04-07 14:39:38 +0000

    Codeforces 1328A. Divisibility Problem

---
+++ A/divisibility_problem.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    var t int
+    fmt.Scan(&t)
+
+    for t > 0 {
+        var a, b, k int64
+        fmt.Scan(&a, &b)
+        k = (a + b - 1) / b
+        b = b * k
+        fmt.Println(b - a)
+        t--
+    }
+}
