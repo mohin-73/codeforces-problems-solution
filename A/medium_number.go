358710e224d4011a283e2236af635adcaa932c73
Eklasur Rahman <mohin73k@gmail.com>
2025-04-17 15:21:33 +0000

    Codeforces 1760A. Medium Number

--- A/medium_number.go
+++ A/medium_number.go
@@ -8,10 +8,10 @@ import (
 func main()  {
     var t int
     fmt.Scan(&t)
+
     for i := 0; i < t; i += 1 {
         arr := make([]int, 3)
         fmt.Scan(&arr[0], &arr[1], &arr[2])
-
         sort.Ints(arr)
         fmt.Println(arr[1])
     }
