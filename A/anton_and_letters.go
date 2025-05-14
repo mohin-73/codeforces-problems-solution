c078b6565f6df99a8a5e285a0930757ebb7db582
Eklas Mohin <mohin73k@gmail.com>
2025-05-14 12:56:58 +0000

    Codeforces 443A. Anton and Letters

--- A/anton_and_letters.go
+++ A/anton_and_letters.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
+    "bufio"
+    "fmt"
+    "os"
 )
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	str, _ := reader.ReadString('\n')
+    var str string
+    reader := bufio.NewReader(os.Stdin)
+    str, _ = reader.ReadString('\n')
 
-	cnt := 0
-	seen := make([]bool, 26)
+    cnt := 0
+    arr := make([]int, 26)
 
-	for i := 0; i < len(str); i++ {
-		if str[i] >= 'a' && str[i] <= 'z' {
-			idx := str[i] - 'a'
-			if !seen[idx] {
-				cnt++
-				seen[idx] = true
-			}
-		}
-	}
+    for i := 0; i < len(str); i++ {
+        if str[i] >= 'a' && str[i] <= 'z' {
+            k := str[i] - 'a'
+            if arr[k] == 0 {
+                cnt++
+                arr[k] = 1
+            }
+        }
+    }
 
-	fmt.Println(cnt)
+    fmt.Println(cnt)
 }
