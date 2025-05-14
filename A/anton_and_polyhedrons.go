c5ac5b7236f0d702cf03cb860717097aff091547
Eklas Mohin <mohin73k@gmail.com>
2025-05-14 13:07:36 +0000

    Codeforces 785A. Anton and Polyhedrons

--- A/anton_and_polyhedrons.go
+++ A/anton_and_polyhedrons.go
@@ -5,25 +5,23 @@ import (
 )
 
 func main() {
-    m := map[string]int {
-        "Tetrahedron": 4,
-        "Cube":        6,
-        "Octahedron":  8,
+    m := map[string]int{
+        "Tetrahedron":  4,
+        "Cube":         6,
+        "Octahedron":   8,
         "Dodecahedron": 12,
-        "Icosahedron": 20,
+        "Icosahedron":  20,
     }
 
     var n int
     fmt.Scan(&n)
-    p := 0
 
-    
-    for n > 0 {
-        var str string
-        fmt.Scan(&str)
-        p = p + m[str]
-        n = n - 1
+    sum := 0
+    for i := 0; i < n; i++ {
+        var shape string
+        fmt.Scan(&shape)
+        sum += m[shape]
     }
 
-    fmt.Println(p)
+    fmt.Println(sum)
 }
