35ac66775cb95199e631ea9d9c8d788de5756a7c
Eklas Mohin <mohin73k@gmail.com>
2025-05-16 14:21:07 +0600

    Codeforces 443A. Anton and Letters

--- A/anton_and_letters.go
+++ A/anton_and_letters.go
@@ -10,18 +10,17 @@ func main() {
     reader := bufio.NewReader(os.Stdin)
     str, _ := reader.ReadString('\n')
 
-    cnt := 0
-    seen := make([]bool, 26)
+    count, seen := 0, make([]bool, 26)
 
     for i := 0; i < len(str); i++ {
         if str[i] >= 'a' && str[i] <= 'z' {
             idx := str[i] - 'a'
             if !seen[idx] {
-                cnt++
+                count += 1
                 seen[idx] = true
             }
         }
     }
 
-    fmt.Println(cnt)
+    fmt.Println(count)
 }
