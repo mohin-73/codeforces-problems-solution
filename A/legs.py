9d08d64e8a46b3b23ba7b45fa6e8050403fc7e75
Eklas Mohin <mohin73k@gmail.com>
2025-10-13 03:43:08 +0600

    Codeforces 1996A - Legs

--- A/legs.py
+++ A/legs.py
@@ -1,8 +1,10 @@
 def main():
     t = int(input())
+
     for _ in range(t):
         legs = int(input())
-        print(int((legs + 3) / 4))
+        print((legs + 3) // 4)
+
 
 if __name__ == "__main__":
     main()
