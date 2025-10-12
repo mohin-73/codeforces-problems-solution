3ee816559bbee068f86edfbfef9a617dd4885c52
Eklas Mohin <mohin73k@gmail.com>
2025-10-13 03:28:13 +0600

    Codeforces 2051B - Journey

--- B/journey.py
+++ B/journey.py
@@ -4,16 +4,15 @@ def main():
     for _ in range(t):
         arr = list(map(int, input().split()))
         s = arr[1] + arr[2] + arr[3] 
-        ans = (int(arr[0] / s)) * 3
-        n = arr[0] % s
+        ans, n = (arr[0] // s) * 3, arr[0] % s
         for i in range(1, 4):
             if n > 0:
                 ans += 1
                 n -= arr[i]
             else:
                 break
-
         print(ans)
+
 
 if __name__ == "__main__":
     main()
