131e700c297a3aca2f61727d97ca3f8e6161f037
Eklas Mohin <mohin73k@gmail.com>
2025-10-27 19:07:32 +0600

    Codeforces 71A - Way Too Long Words

--- A/way_too_long_words.cpp
+++ A/way_too_long_words.cpp
@@ -2,17 +2,19 @@
 using namespace std;
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int n;
     cin >> n;
-    while (n--) {
+    for (int i = 0; i < n; ++i) {
         string str;
         cin >> str;
         if (str.length() <= 10) {
             cout << str << '\n';
             continue;
         }
-        auto len = str.length();
-        cout << str[0] << (len - 2) << str[len - 1] << '\n';
+        auto l = str.length();
+        cout << str[0] << l - 2 << str[l - 1] << '\n';
     }
     return 0;
 }
