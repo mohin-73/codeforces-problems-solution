f54cdb3db3a792b251ffb3e5cceb2023b8b0a725
Eklas Mohin <mohin73k@gmail.com>
2025-11-02 00:15:14 +0600

    Codeforces 1915A - A. Odd One Out

--- A/odd_one_out.cpp
+++ A/odd_one_out.cpp
@@ -2,20 +2,33 @@
 
 using namespace std;
 
-void solve() {
-    int a, b, c;
-    cin >> a >> b >> c;
-    int unique = (a ^ b) ^ c;
-    cout << unique << '\n';
+int solve(int a, int b, int c) {
+    return a ^ b ^ c;
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
+    int t, a, b, c;
     cin >> t;
     while (t--) {
-        solve();
+        cin >> a >> b >> c;
+        cout << solve(a, b, c) << '\n';
     }
     return 0;
 }
+
+/*
+─────────────────────────────────────────────
+    XOR (Exclusive OR) — Key Properties
+─────────────────────────────────────────────
+    1. x ^ x = 0  
+    → Any number XOR itself becomes 0.
+─────────────────────────────────────────────
+    2. x ^ 0 = x  
+    → XOR with 0 keeps the number unchanged.
+─────────────────────────────────────────────
+    3. XOR is Commutative and Associative  
+    → a ^ b ^ c = b ^ c ^ a = (a ^ b) ^ c
+─────────────────────────────────────────────
+*/
