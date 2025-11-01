dd0827e5ce1ab4daa282b1fddc0fbd35b42476d7
Eklas Mohin <mohin73k@gmail.com>
2025-11-02 00:04:29 +0600

    Codeforces 1915A - A. Odd One Out

---
+++ A/odd_one_out.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int a, b, c;
+    cin >> a >> b >> c;
+    int unique = (a ^ b) ^ c;
+    cout << unique << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
