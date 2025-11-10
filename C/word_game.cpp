ce75e31cf75b089979b480446a4e4b6fb56beccd
Eklas Mohin <mohin73k@gmail.com>
2025-11-11 03:27:12 +0600

    Codeforces 1722C - Word Game

--- C/word_game.cpp
+++ C/word_game.cpp
@@ -1,54 +1,56 @@
-#include <iostream>
-#include <vector>
-#include <map>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<string> v1(n);
+    vector<string> v2(n);
+    vector<string> v3(n);
+    map<string, int> mp;
+    int a = 0, b = 0, c = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> v1[i];
+        mp[v1[i]] += 1;
+    }
+    for (int i = 0; i < n; ++i) {
+        cin >> v2[i];
+        mp[v2[i]] += 1;
+    }
+    for (int i = 0; i < n; ++i) {
+        cin >> v3[i];
+        mp[v3[i]] += 1;
+        if (mp[v3[i]] == 1) {
+            c += 3;
+        } else if (mp[v3[i]] == 2) {
+            c += 1;
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        if (mp[v1[i]] == 1) {
+            a += 3;
+        } else if (mp[v1[i]] == 2) {
+            a += 1;
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        if (mp[v2[i]] == 1) {
+            b += 3;
+        } else if (mp[v2[i]] == 2) {
+            b += 1;
+        }
+    }
+    cout << a << ' ' << b << ' ' << c << '\n';
+}
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
-    int t{};
-    std::cin >> t;
-
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
     while (t--) {
-        int n{};
-        std::cin >> n;
-
-        std::vector<std::string> v1(n);
-        std::vector<std::string> v2(n);
-        std::vector<std::string> v3(n);
-        std::map<std::string, int> mp;
-
-        int a{}, b{}, c{};
-
-        for (int i = 0; i < n; ++i) {
-            std::cin >> v1[i];
-            ++mp[v1[i]];
-        }
-        
-        for (int i = 0; i < n; ++i) {
-            std::cin >> v2[i];
-            ++mp[v2[i]];
-        }
-
-        for (int i = 0; i < n; ++i) {
-            std::cin >> v3[i];
-            ++mp[v3[i]];
-            if (mp[v3[i]] == 1) c += 3;
-            else if (mp[v3[i]] == 2) c += 1;
-        }
-
-        for (int i = 0; i < n; ++i) {
-            if (mp[v1[i]] == 1) a += 3;
-            else if (mp[v1[i]] == 2) a += 1;
-        }
-
-        for (int i = 0; i < n; ++i) {
-            if (mp[v2[i]] == 1) b += 3;
-            else if (mp[v2[i]] == 2) b += 1;
-        }
-
-        std::cout << a << " " << b << " " << c << '\n';
+        solve();
     }
-
     return 0;
 }
