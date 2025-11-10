87faa95222fe63a7de986e1db645ebad2f16c5fb
Eklas Mohin <mohin73k@gmail.com>
2025-11-11 05:48:26 +0600

    Codeforces 1722C - Word Game

--- C/word_game.cpp
+++ C/word_game.cpp
@@ -5,49 +5,27 @@ using namespace std;
 void solve() {
     int n;
     cin >> n;
-    vector<string> v1(n);
-    vector<string> v2(n);
-    vector<string> v3(n);
+    vector<vector<string>> v(3, vector<string>(n));
     map<string, int> mp;
-    int a = 0, b = 0, c = 0;
-    for (int i = 0; i < n; ++i) {
-        cin >> v1[i];
-        mp[v1[i]] += 1;
-    }
-    for (int i = 0; i < n; ++i) {
-        cin >> v2[i];
-        mp[v2[i]] += 1;
-    }
-    for (int i = 0; i < n; ++i) {
-        cin >> v3[i];
-        mp[v3[i]] += 1;
-        if (mp[v3[i]] == 1) {
-            c += 3;
-        } else if (mp[v3[i]] == 2) {
-            c += 1;
+    for (auto& x : v) {
+        for (auto& s : x) {
+            cin >> s;
+            mp[s] += 1;
         }
     }
-    for (int i = 0; i < n; ++i) {
-        if (mp[v1[i]] == 1) {
-            a += 3;
-        } else if (mp[v1[i]] == 2) {
-            a += 1;
+    for (int i = 0; i < 3; ++i) {
+        int score = 0;
+        for (auto& s : v[i]) {
+            score += (mp[s] == 1 ? 3 : mp[s] == 2 ? 1 : 0);
         }
+        cout << score << " \n"[i == 2];
     }
-    for (int i = 0; i < n; ++i) {
-        if (mp[v2[i]] == 1) {
-            b += 3;
-        } else if (mp[v2[i]] == 2) {
-            b += 1;
-        }
-    }
-    cout << a << ' ' << b << ' ' << c << '\n';
 }
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    int t;
+    int t; 
     cin >> t;
     while (t--) {
         solve();
