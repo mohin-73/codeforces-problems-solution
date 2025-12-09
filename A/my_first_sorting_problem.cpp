1f6c736d524c67ed9d1d2a88f93ac3d096b7bb3b
Eklas Mohin <mohin73k@gmail.com>
2025-12-09 13:28:13 +0600

    Codeforces 1971A - My First Sorting Problem

---
+++ A/my_first_sorting_problem.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+    int a, b;
+    cin >> a >> b;
+    cout << min(a, b) << ' ' << max(a, b) << '\n';
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
