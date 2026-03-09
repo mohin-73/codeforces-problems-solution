9b939e3351bbd824a27162c2573e0cf8a78def0c
Eklas Mohin <mohin73k@gmail.com>
2026-03-10 01:28:33 +0600

    Codeforces 1527A - And Then There Were K

--- A/and_then_there_were_k.py
+++ A/and_then_there_were_k.py
@@ -1,14 +1,7 @@
-def main():
-    t = int(input())
-
-    for _ in range(t):
-        n = int(input())
-        p = -1
-        while n:
-            p += 1
-            n >>= 1
-        print((2 ** p) - 1)
-
-
-if __name__ == "__main__":
-    main()
+for _ in range(int(input())):
+    n = int(input())
+    p = 0
+    while n > 1:
+        p += 1
+        n >>= 1
+    print(2 ** p - 1)
