40a18e5a31730bf4f645458f8458953f66e4707d
Eklas Mohin <mohin73k@gmail.com>
2026-03-10 00:01:02 +0600

    Codeforces 1999A - A+B Again

--- A/a_plus_b_again.cpp
+++ A/a_plus_b_again.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
 
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
-    short t{};
-    std::cin >> t;
-
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    short t;
+    cin >> t;
     while (t--) {
-        short n{};
-        std::cin >> n;
-        std::cout << (n % 10) + ((n / 10) % 10) << '\n';
+        short n;
+        cin >> n;
+        cout << (n % 10) + ((n / 10) % 10) << '\n';
     }
-    
     return 0;
 }
