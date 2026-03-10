a426c6e09820898e20fb4b87fe325747a25994b2
Eklas Mohin <mohin73k@gmail.com>
2026-03-10 12:06:20 +0600

    Codeforces 1706A - Another String Minimization Problem

--- A/another_string_minimization_problem.py
+++ A/another_string_minimization_problem.py
@@ -1,18 +1,10 @@
-def main():
-    t = int(input())
-
-    for _ in range(t):
-        n, m = map(int, input().split())
-        arr = list(map(int, input().split()))
-        array = ["B"] * m
-        for j in arr:
-            j, k = min(j - 1, m - j), max(j - 1, m - j)
-            if array[j] == "B":
-                array[j] = "A"
-            else:
-                array[k] = "A"
-        print(*array, sep="")
-
-
-if __name__ == "__main__":
-    main()
+for _ in range(int(input())):
+    n, m = map(int, input().split())
+    a = list(map(int, input().split()))
+    s = ['B'] * m
+    for x in a:
+        y = m - x
+        x = x - 1
+        l, r = min(x, y), max(x, y)
+        s[l if s[l] == 'B' else r] = 'A'
+    print(''.join(s))
