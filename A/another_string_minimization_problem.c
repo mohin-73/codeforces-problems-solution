4c1ac22bbad58e30bc1aaee50944d10c7950bca3
Eklas Mohin <mohin73k@gmail.com>
2026-03-11 17:08:27 +0600

    Codeforces 1706A - Another String Minimization Problem

---
+++ A/another_string_minimization_problem.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void solve() {
+    int n, m, a, b;
+    scanf("%d %d", &n, &m);
+    char s[m + 1];
+    for (int i = 0; i < m; i++) {
+        s[i] = 'B';
+    }
+    s[m] = '\0';
+    for (int i = 0; i < n; ++i) {
+        scanf("%d", &a);
+        b = m - a--;
+        int l = a < b ? a : b;
+        int r = a > b ? a : b;
+        s[l] == 'B' ? (s[l] = 'A') : (s[r] = 'A');
+    }
+    printf("%s\n", s);
+}
+
+int main(void) {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
