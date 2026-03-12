5e766f4db77a39a0058ee36a5c43449024b4cf89
Eklas Mohin <mohin73k@gmail.com>
2026-03-12 14:35:16 +0600

    Codeforces 2169A - Alice and Bob

--- A/alice_and_bob.py
+++ A/alice_and_bob.py
@@ -5,9 +5,7 @@ def solve():
     n, a = map(int, input().split())
     arr = list(map(int, input().split()))
     l = r = 0
-    for v in arr:
-        if v == a:
-            continue
+    for v in (x for x in arr if x != a):
         x, y = abs(v - a - 1), abs(v - a + 1)
         l, r = (l + 1, r) if x > y else (l, r + 1)
     print(a - 1 if l > r else a + 1)
