43c73d1d8664d20cc22aba42f590e819409271b5
Eklas Mohin <mohin73k@gmail.com>
2026-03-12 13:27:49 +0600

    Codeforces 1857A - Array Coloring

--- A/array_coloring.py
+++ A/array_coloring.py
@@ -1,6 +1,11 @@
-for _ in range(int(input())):
+import sys
+input = sys.stdin.readline
+
+def solve():
     n = int(input())
     a = list(map(int, input().split()))
     s = sum(a)
     print("YES" if s % 2 == 0 else "NO")
 
+for _ in range(int(input())):
+    solve()
