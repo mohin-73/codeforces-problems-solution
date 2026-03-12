8ebda84272ecd32c530451313d20728c1905b2bf
Eklas Mohin <mohin73k@gmail.com>
2026-03-12 13:27:16 +0600

    Codeforces 1537A - Arithmetic Array

--- A/arithmetic_array.py
+++ A/arithmetic_array.py
@@ -1,6 +1,12 @@
-for _ in range(int(input())):
+import sys
+input = sys.stdin.readline
+
+def solve():
     n = int(input())
     a = list(map(int, input().split()))
     s = sum(a)
     m = 0 if n == s else 1 if (s < 0 or n > s) else s - n
     print(m)
+
+for _ in range(int(input())):
+    solve()
