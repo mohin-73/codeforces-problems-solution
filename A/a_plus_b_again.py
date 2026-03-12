fbe1337ac362911b87535979220f18a14f8d5dec
Eklas Mohin <mohin73k@gmail.com>
2026-03-12 13:24:38 +0600

    Codeforces 1999A - A+B Again

--- A/a_plus_b_again.py
+++ A/a_plus_b_again.py
@@ -1,3 +1,9 @@
-for _ in range(int(input())):
+import sys
+input = sys.stdin.readline
+
+def solve():
     n = int(input())
     print(n % 10 + n // 10)
+
+for _ in range(int(input())):
+    solve()
