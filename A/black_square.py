7d52a7bc98923725e9f7254f04222f340213b618
Eklas Mohin <mohin73k@gmail.com>
2026-03-12 13:08:55 +0600

    Codeforces 431A - Black Square

--- A/black_square.py
+++ A/black_square.py
@@ -1,6 +1,13 @@
-a = list(map(int, input().split()))
-s = input().strip()
-w = 0
-for ch in s:
-    w += a[ord(ch) - ord('1')]
-print(w)
+import sys
+input = sys.stdin.readline
+
+def solve():
+    a = list(map(int, input().split()))
+    s = input().strip()
+    w = 0
+    for ch in s:
+        w += a[ord(ch) - ord('1')]
+    print(w)
+
+for _ in range(int(input())):
+    solve()
