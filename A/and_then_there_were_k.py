d3cde7e103fd9e687cc043917d06533fd01fbdcb
Eklas Mohin <mohin73k@gmail.com>
2026-03-12 13:25:43 +0600

    Codeforces 1527A - And Then There Were K

--- A/and_then_there_were_k.py
+++ A/and_then_there_were_k.py
@@ -1,7 +1,13 @@
-for _ in range(int(input())):
+import sys
+input = sys.stdin.readline
+
+def solve():
     n = int(input())
     p = 0
     while n > 1:
         p += 1
         n >>= 1
     print(2 ** p - 1)
+
+for _ in range(int(input())):
+    solve()
