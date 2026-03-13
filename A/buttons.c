28fa2286c755806b819fbc4a8498fce33e96a33c
Eklas Mohin <mohin73k@gmail.com>
2026-03-13 11:45:35 +0600

    Codeforces 1858A - Buttons

--- A/buttons.c
+++ A/buttons.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
-int main() {
-    int t, a, b, c;
+void solve() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    int first = a + (c + 1) / 2;
+    int second = b + c / 2;
+    puts((first > second ? "First" : "Second"));
+}
+
+int main(void) {
+    int t;
     scanf("%d", &t);
-    for (int i = 0; i < t; ++i) {
-        scanf("%d %d %d", &a, &b, &c);
-        int first = a + (c + 1) / 2;
-        int second = b + c / 2;
-        puts((first > second ? "First" : "Second"));
+    while (t--) {
+        solve();
     }
     return 0;
 }
--- A/buttons.py
+++ A/buttons.py
@@ -1,4 +1,10 @@
-for _ in range(int(input())):
+import sys
+input = sys.stdin.readline
+
+def solve():
     a, b, c = map(int, input().split())
     first, second = a + (c + 1) // 2, b + c // 2
     print("First" if first > second else "Second")
+
+for _ in range(int(input())):
+    solve()
